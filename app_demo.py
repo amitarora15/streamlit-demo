e33a16637c849dcfe7549ad87a52d417cc71bd68
Amit Arora <amarora@tibco.com>
2021-10-22 07:50:25 +0530

    removed dependencies

--- app_demo.py
+++ app_demo.py
@@ -1,44 +1,28 @@
 import streamlit as st
+import streamlit_pages.streamlit_pages import MultiPage
 
-from pages import home, about, contact
+def home():
+    st.write("Welcome to home page")
+    if st.button("Click Home"):
+        st.write("Welcome to home page")
 
-st.markdown(
-    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">',
-    unsafe_allow_html=True,
-)
-query_params = st.experimental_get_query_params()
-tabs = ["Home", "About", "Contact"]
-if "tab" in query_params:
-    active_tab = query_params["tab"][0]
-else:
-    active_tab = "Home"
 
-if active_tab not in tabs:
-    st.experimental_set_query_params(tab="Home")
-    active_tab = "Home"
+def about():
+:wq    st.write("Welcome to about page")
+    if st.button("Click about"):
+        st.write("Welcome to About page")
 
-li_items = "".join(
-    f"""
-    <li class="nav-item">
-        <a class="nav-link{' active' if t==active_tab else ''}" href="/?tab={t}">{t}</a>
-    </li>
-    """
-    for t in tabs
-)
-tabs_html = f"""
-    <ul class="nav nav-tabs">
-    {li_items}
-    </ul>
-"""
 
-st.markdown(tabs_html, unsafe_allow_html=True)
-st.markdown("<br>", unsafe_allow_html=True)
+def contact():
+    st.write("Welcome to contact page")
+    if st.button("Click Contact"):
+        st.write("Welcome to contact page")
 
-if active_tab == "Home":
-    home.write()
-elif active_tab == "About":
-    about.write()
-elif active_tab == "Contact":
-    contact.write()
-else:
-    st.error("Something has gone terribly wrong.")
+
+# call app class object
+app = MultiPage()
+# Add pages
+app.add_page("Home",home)
+app.add_page("About",about)
+app.add_page("Contact",contact)
+app.run()
