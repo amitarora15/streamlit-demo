3a356655639a35a8676512f6b0b36ea6fa6b07df
Amit Arora <amarora@tibco.com>
2021-10-22 07:41:46 +0530

    Initial Commit

---
+++ app_demo.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+from pages import home, about, contact
+
+st.markdown(
+    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">',
+    unsafe_allow_html=True,
+)
+query_params = st.experimental_get_query_params()
+tabs = ["Home", "About", "Contact"]
+if "tab" in query_params:
+    active_tab = query_params["tab"][0]
+else:
+    active_tab = "Home"
+
+if active_tab not in tabs:
+    st.experimental_set_query_params(tab="Home")
+    active_tab = "Home"
+
+li_items = "".join(
+    f"""
+    <li class="nav-item">
+        <a class="nav-link{' active' if t==active_tab else ''}" href="/?tab={t}">{t}</a>
+    </li>
+    """
+    for t in tabs
+)
+tabs_html = f"""
+    <ul class="nav nav-tabs">
+    {li_items}
+    </ul>
+"""
+
+st.markdown(tabs_html, unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+
+if active_tab == "Home":
+    home.write()
+elif active_tab == "About":
+    about.write()
+elif active_tab == "Contact":
+    contact.write()
+else:
+    st.error("Something has gone terribly wrong.")
